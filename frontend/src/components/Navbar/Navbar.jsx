8d8fbd3d7ec681f92ead58d5f3e992b3ccc5d91b
Eugine Onyango <eugineogutu004@gmail.com>
2025-10-26 20:13:39 +0300

    Modified nav bar list to impelement the use state const for hover

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
 import './Navbar.css'   
 import { assets } from '../../assets/assets'
 const Navbar = () => {
+
+  const [menu,setMenu] = useState("Home"); 
   return (
     <div className="navbar">
         <img src={assets.logo} alt="" className='logo' />
         <ul className="navbar-menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Menu</a></li>
-            <li><a href="#">Mobile-app</a></li>
-            <li><a href="#">Contact us</a></li>
+            <li className={menu==="Home"?"active":""}>Home</li>
+            <li className={menu==="Menu"?"active":""}>Menu</li>
+            <li className={menu==="Mobile-app"?"active":""}>Mobile-app</li>
+            <li className={menu==="Contact us"?"active":""}>Contact us</li>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt=""/>
             <div className="navbar-search-icon">
-                <img src={assets.basket} alt=""/>
+                <img src={assets.basket_icon} alt=""/>
                 <div className="dot"></div>
             </div>
             <button>sign in</button>
