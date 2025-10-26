766d97557a8be0a8576af2d0977631a88df8bb63
Eugine Onyango <eugineogutu004@gmail.com>
2025-10-26 14:41:33 +0300

    Added the css for the navbar components and the global css font family

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,14 +5,14 @@ const Navbar = () => {
   return (
     <div className="navbar">
         <img src={assets.logo} alt="" className='logo' />
-        <ul className="nav-links">
+        <ul className="navbar-menu">
             <li><a href="#">Home</a></li>
             <li><a href="#">Menu</a></li>
             <li><a href="#">Mobile-app</a></li>
             <li><a href="#">Contact us</a></li>
         </ul>
         <div className="navbar-right">
-            <img src={assets.search} alt="search" className='icon' />
+            <img src={assets.search_icon} alt=""/>
             <div className="navbar-search-icon">
                 <img src={assets.basket} alt=""/>
                 <div className="dot"></div>
